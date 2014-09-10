646174c9ebfe90464c99f2788b5328f0c718f68c
ion-storm <defconoii@gmail.com>
2014-09-09 21:58:55 -0400

    Revert "msm: kgsl: Prevent device start after it is already started"
    This reverts commit 8421e456b08ec151ad6f38228fd17ab1c3e57768.

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1673,9 +1673,6 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	unsigned int state = device->state;
 	unsigned int regulator_left_on = 0;
 
-	if (test_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv))
-		return 0;
-
 	kgsl_cffdump_open(device);
 
 	kgsl_pwrctrl_set_state(device, KGSL_STATE_INIT);
@@ -1740,8 +1737,6 @@ static int _adreno_start(struct adreno_device *adreno_dev)
 	adreno_dispatcher_start(device);
 
 	device->reset_counter++;
-
-	set_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
 
 	return 0;
 
@@ -1837,8 +1832,6 @@ static int adreno_stop(struct kgsl_device *device)
 	kgsl_pwrctrl_disable(device);
 
 	kgsl_cffdump_close(device);
-
-	clear_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
 
 	return 0;
 }
